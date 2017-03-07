6fcb80e9cd74fda1789b111a7f5593355c97bfa6
David <david@templatious.org>
2017-03-07 20:14:56 +0200

    version update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slothtest "0.5.14"
+(defproject slothtest "0.5.15"
   :description "Slothtest - simple automatic testing solution"
   :url "https://github.com/davidkazlauskas/slothtest"
   :license {:name "Eclipse Public License"
